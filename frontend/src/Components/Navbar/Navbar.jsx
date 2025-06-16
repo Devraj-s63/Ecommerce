ac281f786fa82826860766a713ff17da9ac35dcb
Devraj-s63 <ds8094848310@gamil.com>
2025-06-16 23:18:15 +0530

    images added

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="Nav-logo">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
