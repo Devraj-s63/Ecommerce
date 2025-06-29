a2b043376d3aa9a19f77b310c32e756971614fb6
Devraj-s63 <ds8094848310@gamil.com>
2025-06-29 17:27:05 +0530

    route creater

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo.webp";
 import cart_icon from "../../assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="Nav-logo">
         <img src={logo} alt="" height="50px" />
         <p>Sellverse</p>
       </div>
-      <ul className='nav-menu'>
-        <li>Home</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("home");
+          }}
+        ><Link to = "/">Home</Link>
+          {menu === "home" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link to = "/mens">Men</Link>
+          {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        ><Link to = "/womens">Women</Link>
+          {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        ><Link to = "/kids">Kids</Link>
+          {menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
-        <img src={cart_icon} alt=""  height="50px"/>
+        <img src={cart_icon} alt="" height="50px" />
         <div className="nav-cart-count">0</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
