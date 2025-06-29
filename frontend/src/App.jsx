953e7a775bfc833c90e41492df8fd89044e20c70
Devraj-s63 <ds8094848310@gamil.com>
2025-06-29 22:40:47 +0530

    banner added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,10 @@ import Product from "./Pages/Product";
 import ShopCategory from "./Pages/ShopCategory";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/loginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./assets/banner.jpg";
+import women_banner from "./assets/women_banner.avif";
+import kids_banner from "./assets/kids_banner.jpg";
 function App() {
   return (
     <div>
@@ -12,14 +16,15 @@ function App() {
 <Navbar/>
 <Routes>
   <Route path="/" element={<Shop/>}/>
-  <Route path="/mens" element={<ShopCategory category="mens"/> }/>
-  <Route path="/womens" element={<ShopCategory category="womens"/>}/>
-  <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+  <Route path="/mens" element={<ShopCategory banner={men_banner} category="mens"/> }/>
+  <Route path="/womens" element={<ShopCategory banner={women_banner} category="womens"/>}/>
+  <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kids"/>}/>
   <Route path="/product" element={<Product/>}/>
   <Route path=":productId" element={<Product/>}/>
   <Route path="/cart" element={<Cart/>}/>
   <Route path="/login" element={<LoginSignup/>}/>
 </Routes>
+<Footer/>
       </BrowserRouter>
     </div>
   )
