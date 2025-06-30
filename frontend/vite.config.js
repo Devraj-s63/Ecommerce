8c5905b1b0d8c5a7f64e568dd0d5cbedc582fca4
Devraj-s63 <ds8094848310@gamil.com>
2025-06-30 07:48:07 +0530

    trying to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Ecommerce/", // 🔁 change this
+  base: "/Ecommerce", // 🔁 change this
   plugins: [react()],
 });
